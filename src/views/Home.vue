dd2fb3d7edaeaff501dee6afd2c7428766eb9d8d
karlosicolombia <karlosicolombia@protonmail.com>
2020-12-10 11:27:56 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
+
+
 <template>
   <div class="center">
-    <div>
+    <div id="hover">
       <h1>Collectors</h1>
-      <ul>
-        <li v-for="i in 3" :key="i">
-          <a @click="setupCollectors(i+1, 'en')">Set up a game of Collectors for {{i+1}} players</a>.
+
+    <div>
+    <ul>
+        <li class ="Engelska" v-for="i in 3" :key="i">
+          <a @click="setupCollectors(i+1, 'en')">  Set up a game of Collectors for {{i+1}} players</a>.
         </li>
       </ul>
+      </div>
+
+    <div>
       <ul>
-        <li v-for="i in 3" :key="i">
+        <li class="Svenska" v-for="i in 3" :key="i">
           <a @click="setupCollectors(i+1, 'se')">Spela Collectors med {{i+1}} spelare</a>.
         </li>
       </ul>
     </div>
+  </div>
   </div>
 </template>
 
@@ -26,13 +34,17 @@ export default {
   },
   methods: {
     setupCollectors: function (playerCount, lang="en") {
-      this.$store.commit('SETUP_GAME', {roomId: this.$store.state.roomId, 
+      this.$store.commit('SETUP_GAME', {roomId: this.$store.state.roomId,
         playerCount: playerCount,
         lang: lang });
       this.$router.push("/room/" + this.$store.state.roomId);
     },
   }
 }
+
+
+
+
 </script>
 <style scoped>
   .center {
@@ -41,5 +53,18 @@ export default {
     justify-content: center;
     height: 100vh;
     padding: 1em;
+
   }
+
+  // These CSS classes makes the texts in the fOr loop change colOR And changes the mousepointer img
+  .Engelska :hover{
+    color: Cyan;
+    cursor: url('https://cdn.freebiesupply.com/logos/large/2x/windows-cursor-logo-png-transparent.png'), pointer;
+  }
+  .Svenska :hover{
+    color: Cyan;
+    cursor: url('https://cdn.freebiesupply.com/logos/large/2x/windows-cursor-logo-png-transparent.png'), pointer;
+  }
+
+
 </style>
