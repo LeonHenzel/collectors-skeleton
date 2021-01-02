2cef6e535e2d879187ee1cf77c076409f39bb8bd
Fredrik <fredrik97nilsson@gmail.com>
2021-01-02 13:44:01 +0100

    bajsbajsbajs

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -146,34 +146,7 @@
 
 
 
-          <div class="otherPlayers">
-            <h2>Stats</h2>
-            <h3>{{labels.currentlyitsround}} {{round}}</h3>
-            <CollectorsOtherPLayers v-if="allPlayersReady"
-            :playerList="playerList"/>
-            <br>
 
-            <h3>{{labels.currentrankings}}</h3>
-            <div v-for="player in players" :key="player">
-            <h4>{{player.playerName}} {{labels.hasthismanypoints}} {{player.points}}</h4>
-            </div>
-
-            <br>
-            <h3>{{labels.whosturnisit}}</h3>
-            <div v-for="player in players" :key="player">
-            <div v-if="player.myTurn">
-
-            <h4>{{labels.its}} {{player.playerName}}'s {{labels.turn}}</h4>
-
-            <br>
-            <h3>{{labels.whosbidding}}</h3>
-
-
-            </div>
-            </div>
-
-
-          </div>
 
           <div>{{players}}</div>
 
