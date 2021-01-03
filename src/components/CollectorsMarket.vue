fb893751ea592382199120d232a2c37aeda1c412
Fredrik <fredrik97nilsson@gmail.com>
2021-01-03 12:38:37 +0100

    RaiseMarket och marketvalues

--- src/components/CollectorsMarket.vue
+++ src/components/CollectorsMarket.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="marketWrapper">
     <h1>{{labels.raiseMarket}}</h1>
     <div class="buttons" v-for="(p, index) in placement" :key="index">
       <button
@@ -13,6 +13,38 @@
         {{p.playerId}}
       </div>
     </div>
+    <div class="marketValuesWrapper">
+      <div class="firstValue">
+        {{marketValues.movie}}
+      </div>
+      <div class="firstValueSym">
+        <img src="/images/movieValue.png" />
+      </div>
+      <div class="secondValue">
+        {{marketValues.music}}
+      </div>
+      <div class="secondValueSym">
+        <img src="/images/musicValue.png" />
+      </div>
+      <div class="thirdValue">
+        {{marketValues.technology}}
+      </div>
+      <div class="thirdValueSym">
+        <img src="/images/techValue.png" />
+      </div>
+      <div class="fourthValue">
+        {{marketValues.figures}}
+      </div>
+      <div class="fourthValueSym">
+        <img src="/images/figursValue.png" />
+      </div>
+      <div class="fithValue">
+        {{marketValues.fastaval}}
+      </div>
+      <div class="fithValueSym">
+        <img src="/images/fastavalValue.png" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,7 +54,8 @@ export default {
   props: {
     labels: Object,
     player: Object,
-    placement:  Array
+    placement:  Array,
+    marketValues: Object
   },
   methods: {
     cannotAfford: function (cost) {
@@ -46,5 +79,73 @@ export default {
 .buttons {
   display: grid;
   grid-template-columns: repeat(auto-fill, 130px);
+  grid-row: 1;
+  grid-column: 1;
 }
+
+.marketWrapper {
+  display: grid;;
+  grid-template-columns: 100%;
+  grid-template-rows: 20%, 60%;
+}
+
+.marketValues {
+  grid-column: 2;
+  grid-row: 1;
+  display: grid;
+  grid-row: 20% 40%;
+  grid-column: 19% 19% 19% 19% 19%;
+}
+
+.firstValue {
+  grid-column: 1;
+  grid-row: 1;
+}
+
+.secondValue {
+  grid-column: 2;
+  grid-row: 1;
+}
+
+.thirdValue {
+  grid-column: 3;
+  grid-row: 1;
+}
+
+.fourthValue {
+  grid-column: 4;
+  grid-row: 1;
+}
+
+.fithValue {
+  grid-column: 5;
+  grid-row: 1;
+}
+
+.firstValueSym {
+  grid-column: 1;
+  grid-row: 2;
+}
+
+.secondValueSym {
+  grid-column: 2;
+  grid-row: 2;
+}
+
+.thirdValueSym {
+  grid-column: 3;
+  grid-row: 2;
+}
+
+.fourthValueSym {
+  grid-column: 4;
+  grid-row: 2;
+}
+
+.fithValueSym {
+  grid-column: 5;
+  grid-row: 2;
+}
+
+
 </style>
