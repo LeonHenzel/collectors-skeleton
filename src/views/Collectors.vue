4f3e4bc16e91395d6f115bd09a865011b5d08973
karlosicolombia <karlosicolombia@protonmail.com>
2021-01-09 17:12:10 +0100

    collectors help reglerna i mitten

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -105,7 +105,7 @@
                 <div class="helpOverlay" id = "expandHelp">
                   <a href="#" class="closeHelpGridButton" @click="minimizeHelpGrid()">&times;</a>
                   <div class="helpInfoOverlay">
-                    <h1 id="helpLogo"><img src="/images/CollectorsLogo01.png" height="500"></h1>
+                    <img src="/images/CollectorsLogo01.png" class="helplogo" height="500">
                     <h1 class="rulebook">{{labels.rulebook}}</h1>
                     <img src="/images/Screenshot 2021-01-05 at 14.43.21.png">
                     <img src="/images/Screenshot 2021-01-05 at 14.48.01.png">
@@ -1896,7 +1896,8 @@ har gjort true eller false. Om man börjar auction så ska auction vara true och
 
     .helpInfoOverlay{
       color: white;
-      align-items: Center;
+      position: relative;
+      left: 11%;
 
       margin-top: -5%;
     }
@@ -3127,6 +3128,10 @@ cursor: not-allowed;
       display: none;
     }
 
+}
+.helplogo{
+  position: relative;
+  left: 11%;
 }
 
 
